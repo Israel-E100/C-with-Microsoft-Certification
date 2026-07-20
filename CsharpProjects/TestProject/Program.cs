1e99afc136d4935c1e2a21264cb2c733292b2868
Jorge Israel <israelworkingithub@gmail.com>
2026-07-19 20:01:08 -0500

    Clear() and Resize()

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,16 +1,46 @@
 ﻿string [] pallets = ["B14", "A11", "B12", "A13"];
+Console.WriteLine("");
 
-Console.WriteLine("Sorted...");
-Array.Sort(pallets);
+//Console.WriteLine($"Before: {pallets[0].ToLower()}");
+Array.Clear(pallets, 0, 2);
+//Console.WriteLine($"After: {pallets[0].ToLower()}");
+Console.WriteLine($"Clearing 2... count: {pallets.Length}");
 foreach(var pallet in pallets)
 {
-    Console.WriteLine($"--{pallet}");
+    Console.WriteLine($"-- {pallet}");
 }
 
 Console.WriteLine("");
-Console.WriteLine("Reserved...");
-Array.Reverse(pallets);
-foreach(var pallet in pallets)
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6... count {pallets.Length}");
+
+pallets[4]= "C01";
+pallets[5]= "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3... count {pallets.Length}");
+foreach (var pallet in pallets)
 {
     Console.WriteLine($"--{pallet}");
 }
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach(var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Reserved...");
+// Array.Reverse(pallets);
+// foreach(var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
