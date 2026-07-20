872612668e6d8fe2825732d0daca8c3296eb4a9e
Jorge Israel <israelworkingithub@gmail.com>
2026-07-19 20:15:10 -0500

    split and join

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,34 +1,34 @@
-﻿string [] pallets = ["B14", "A11", "B12", "A13"];
-Console.WriteLine("");
+﻿// string [] pallets = ["B14", "A11", "B12", "A13"];
+// Console.WriteLine("");
 
-//Console.WriteLine($"Before: {pallets[0].ToLower()}");
-Array.Clear(pallets, 0, 2);
-//Console.WriteLine($"After: {pallets[0].ToLower()}");
-Console.WriteLine($"Clearing 2... count: {pallets.Length}");
-foreach(var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+// //Console.WriteLine($"Before: {pallets[0].ToLower()}");
+// Array.Clear(pallets, 0, 2);
+// //Console.WriteLine($"After: {pallets[0].ToLower()}");
+// Console.WriteLine($"Clearing 2... count: {pallets.Length}");
+// foreach(var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
 
-Console.WriteLine("");
-Array.Resize(ref pallets, 6);
-Console.WriteLine($"Resizing 6... count {pallets.Length}");
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6... count {pallets.Length}");
 
-pallets[4]= "C01";
-pallets[5]= "C02";
+// pallets[4]= "C01";
+// pallets[5]= "C02";
 
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
 
-Console.WriteLine("");
-Array.Resize(ref pallets, 3);
-Console.WriteLine($"Resizing 3... count {pallets.Length}");
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"--{pallet}");
-}
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3... count {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
 
 // Console.WriteLine("Sorted...");
 // Array.Sort(pallets);
@@ -44,3 +44,16 @@ foreach (var pallet in pallets)
 // {
 //     Console.WriteLine($"--{pallet}");
 // }
+
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+//string result = new string (valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
